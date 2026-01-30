24c8cd9d95b2f7d64466caf73d12229a914419c2
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-01-30 15:04:47 +0200

    Command-line arguments

---
+++ 11_01-writefile.py
@@ -0,0 +1,16 @@
+# On the terminal: python3 11_01-writefile.py -o 11_01-file.txt -t "Some text to write in the tst file"
+
+from argparse import ArgumentParser 
+
+parser = ArgumentParser()
+
+parser.add_argument('--output', '-o', required=True, help='The destination file for the output of this program')
+parser.add_argument('--text', '-t', required=True, help='The text to write to the file')
+
+
+args = parser.parse_args()
+
+with open(args.output, 'w') as f:
+    f.write(args.text+'\n')
+
+print(f'Wrote "{args.text}" to file "{args.output}"')
